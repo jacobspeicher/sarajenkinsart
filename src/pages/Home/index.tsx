723f3240cc4ff3d67067dc2fb19808f6cee11c20
Jacob Speicher <speicherjacob@gmail.com>
2024-12-11 21:00:26 -0700

    put images in table

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,31 @@ import './style.css';
 import { Artwork } from '../../assets';
 
 export function Home() {
+	const rows = [];
+	for (let i = 0; i < Artwork.length; i += 2) {
+		const first = Artwork[i];
+		let second = null;
+		if (i + 1 < Artwork.length) {
+			second = Artwork[i + 1];
+		}
+		rows.push(
+			<tr>
+				<td><Art source={first.source} name={first.name} /></td>
+				{
+					second ? 
+					<td><Art source={second.source} name={second.name} /></td> :
+					<td></td>
+				}
+			</tr>
+		);
+	}
+
 	return (
 		<div class="home">
 			<h2>Sara Jenkins makes art.</h2>
-			{
-				Artwork.map(({name, source}) => {
-					return <Art source={source} name={name} />
-				})
-			}
+			<table>
+				{rows}
+			</table>
 		</div>
 	);
 }
@@ -19,11 +36,11 @@ export function Home() {
 function Art(props) {
 	const artPath = `/${props.source}.jpg`;
 	return (
-		<Fragment>
+		<div className='artwork'>
 			<img src={artPath} />
 			{/* hover to view title */}
 			<h3>{props.name}</h3>
-		</Fragment>
+		</div>
 	)
 }
 
